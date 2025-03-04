d3733717264caa4e40884e3cb630b7b744ebcc8f
Robert Fu <robert.fu@telus.com>
2025-03-04 13:21:53 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+
 function App() {
+  const [ value, setValue ] = useState(0)
+  function increase(){
+    setValue(prev => prev + 1)
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         You have clicked { value } time(s)
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button
+          onClick={increase}>
+          Click Me!
+        </button>
       </header>
     </div>
   );
